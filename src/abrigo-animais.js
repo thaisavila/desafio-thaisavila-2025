05cf68df78ef58886618f73bc6f5f8b87d0260be
dalessandro-dev <daleconta1@gmail.com>
2025-09-10 01:10:37 -0300

    init: Primeira solução  do desafio

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,221 +1,174 @@
 class AbrigoAnimais {
-  encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
-    // Tratamento de dados e variáveis globais:
-    // Lista de animais e brinquedos válidos
-    const animaisValidos = ["Rex", "Mimi", "Fofo", "Zero", "Bola", "Bebe", "Loco"]
-    const brinquedosValidos = ["RATO", "BOLA", "LASER", "CAIXA", "NOVELO", "SKATE"]
+  encontraPessoas(brinquedosPessoaUm, brinquedosPessoaDois, animaisDesejados) {
+      //Funções que retornam False se não passarem nas validações ou True se passarem
+      //Confere se há repetições
+      function confereRepeticao(obj) {  
+          const array = [...new Set(obj)]
+      
+          if (obj.length !== array.length) return false
 
-    // Transformando entradas em listas
-    const listaBrinquedosPessoa1 = brinquedosPessoa1.trim().split(",")
-    const listaBrinquedosPessoa2 = brinquedosPessoa2.trim().split(",")
-    const listaAnimais = ordemAnimais.trim().split(",")
-    // Transformando listas em sets
-    let setBrinquedosPessoa1 = new Set(listaBrinquedosPessoa1)
-    let setBrinquedosPessoa2 = new Set(listaBrinquedosPessoa2)
-
-    // Objeto com animais e seus brinquedos preferidos
-    const animais = {
-      Rex: { animal: "cão", brinquedos: ["RATO", "BOLA"] },
-      Mimi: { animal: "gato", brinquedos: ["BOLA", "LASER"] },
-      Fofo: { animal: "gato", brinquedos: ["BOLA", "RATO", "LASER"] },
-      Zero: { animal: "gato", brinquedos: ["RATO", "BOLA"] },
-      Bola: { animal: "cão", brinquedos: ["CAIXA", "NOVELO"] },
-      Bebe: { animal: "cão", brinquedos: ["LASER", "RATO", "BOLA"] },
-      Loco: { animal: "jabuti", brinquedos: ["SKATE", "RATO"] },
-    };
-
-    let pessoa1PodeAdotar = []
-    let pessoa2PodeAdotar = []
-
-
-    // Validar entradas:
-    // Função para verificar se brinquedos ou animais são válidos
-    function validarBrinquedosOuAnimais(escolhaDaPessoa, listaValidos,) {
-      if (escolhaDaPessoa.some(e => !listaValidos.includes(e))) {
-        return true
+          return true
       }
-    }
 
-    // Chamar as funções e parar o código se der erro
-    let resultado = validarBrinquedosOuAnimais(listaBrinquedosPessoa1, brinquedosValidos)
-
-    if (resultado) {
-      return { erro: 'Brinquedo inválido' }
-    }
-    resultado = validarBrinquedosOuAnimais(listaBrinquedosPessoa2, brinquedosValidos)
-    if (resultado) {
-      return { erro: 'Brinquedo inválido' }
-    }
-    resultado = validarBrinquedosOuAnimais(listaAnimais, animaisValidos)
-    console.log(resultado)
-    if (resultado) {
-      return { erro: 'Animal inválido' }
-    }
-
-    // Verificar se os brinquedos estão repetidos
-    if (setBrinquedosPessoa1.size != listaBrinquedosPessoa1.length) {
-      return { erro: 'Brinquedo inválido' }
-    }
-    if (setBrinquedosPessoa2.size != listaBrinquedosPessoa2.length) {
-      return { erro: 'Brinquedo inválido' }
-    }
-
-    /// Minha function de verificacao e gatos
-    function verificarBrinquedosPreferidos() {
-      let brinquedosAdotadosPessoa1 = new Set()
-      let brinquedosAdotadosPessoa2 = new Set()
-
-      for (let i of listaAnimais) {
-        let preferidos = animais[i].brinquedos // Lista com os brinquedos preferidos de cada animal escolhido
-        let conflito = preferidos.some(b => brinquedosAdotadosPessoa1.has(b)) // .has verifica se b ta no set e .some verifica se pelo menos um desses é true
-
-        let preferidosIntersecçãoPessoa1 = listaBrinquedosPessoa1.filter(a => preferidos.includes(a)) // Cria uma nova lista com a intersecçãode pessoa1PodeAdotar e preferidos 
-
-        if (preferidos.join() === preferidosIntersecçãoPessoa1.join()) { // Compara se a nova lista é igual preferidos
-          if (animais[i].animal === "gato") { // Verifica se os animais são gatos
-            if (conflito) { // Se sim e tiver conflito, muda pra false
-              pessoa1PodeAdotar.push(false)
-            } else { // Se não tem conflito, permite ser true
-              pessoa1PodeAdotar.push(true)
-              preferidos.forEach(b => brinquedosAdotadosPessoa1.add(b))
-            }
-          } else { // Se não é gato = true
-            pessoa1PodeAdotar.push(true)
-            preferidos.forEach(b => brinquedosAdotadosPessoa1.add(b))
+      //Confere se os brinquedos informados são válidos
+      function validaBrinquedo(brinquedos) {
+          for (let brinquedo of brinquedos) {
+              if (!(listaBrinquedos.includes(brinquedo))) return false
           }
-        } else { // Se a ordem ta errada = false
-          pessoa1PodeAdotar.push(false)
-        }
 
-        let conflito2 = preferidos.some(b => brinquedosAdotadosPessoa2.has(b))
-
-        let preferidosIntersecçãoPessoa2 = listaBrinquedosPessoa2.filter(a => preferidos.includes(a))
-
-        if (preferidos.join() === preferidosIntersecçãoPessoa2.join()) {
-          if (animais[i].animal === "gato") {
-            if (conflito2) {
-              pessoa2PodeAdotar.push(false)
-            } else {
-              pessoa2PodeAdotar.push(true)
-              preferidos.forEach(b => brinquedosAdotadosPessoa2.add(b))
-            }
-          } else {
-            pessoa2PodeAdotar.push(true)
-            preferidos.forEach(b => brinquedosAdotadosPessoa2.add(b))
-          }
-        } else {
-          pessoa2PodeAdotar.push(false)
-        }
+          return true
       }
-    }
 
-    function loco() {
-      if (listaAnimais.includes('Loco')) { //Verifica se a pessoa escolheu o Loco
-        let indice
-        for (let i in listaAnimais) {
-          if (listaAnimais[i] === 'Loco') {
-            indice = i
-          }
-          if (pessoa1PodeAdotar[indice] === false) {
-            if (pessoa1PodeAdotar.includes(true)) {
-              if (listaBrinquedosPessoa1.includes('SKATE') && listaBrinquedosPessoa1.includes('RATO'))
-                pessoa1PodeAdotar[indice] = true
-            }
-          } else {
-            let cont = 0
-            for (let a of pessoa1PodeAdotar) {
-              if (a === true) {
-                cont += 1
+      //Transforma os parâmetros em array
+      function transformaEmArray(obj) {
+          return obj.split(",")
+      }
+
+      //Função geral que verifica se os brinquedos da pessoa são os que o animal gosta
+      function casoGeral(brinquedos, animal) {
+          const animalBrinquedos = listaAnimais[animal].brinquedos
+
+          //Verifica se os brinquedos da pessoa bate com os brinquedos do animal
+          //Remove os brinquedos que o animal não gosta para comparar depois
+          for (let brinquedo of brinquedos) {
+              if (!animalBrinquedos.includes(brinquedo)) {
+                  brinquedos = brinquedos.filter(brinquedoFiltrado => brinquedoFiltrado !== brinquedo)
               }
-            }
-            if (cont <=1) {
-              pessoa1PodeAdotar[indice] = false
-            }
           }
-          if (pessoa2PodeAdotar[indice] === false) {
-            if (pessoa2PodeAdotar.includes(true)) {
-              if (listaBrinquedosPessoa2.includes('SKATE') && listaBrinquedosPessoa2.includes('RATO'))
-                pessoa2PodeAdotar[indice] = true
-            }
-          } else {
-            let cont = 0
-            for (let a of pessoa2PodeAdotar) {
-              if (a === true) {
-                cont += 1
+
+          //Compara se o array que foi retirado os brinquedos que não gosta é igual aos brinquedos que o animal gosta
+          if (!animalBrinquedos.every((brinquedo, i) => brinquedo === brinquedos[i])) return false
+          
+          return true
+      }
+
+      //Função que aplica as regras específicas quando o animal é um gato
+      function casoGato(brinquedosPessoa, animaisAdotados, animalNome) {
+          if (!casoGeral(brinquedosPessoa, animalNome)) return
+
+          //Remove todos os brinquedos da lista dos brinquedos da pessoa que o gato usa, pois ele não divide, logo é como se não existisse mais
+          for (let brinquedo of listaAnimais[animalNome].brinquedos) {
+              if (brinquedosPessoa.includes(brinquedo)) {
+                  let index = brinquedosPessoa.indexOf(brinquedo)
+                  brinquedosPessoa.splice(index, 1)
               }
-            }
-            if (cont <= 1) {
-              pessoa2PodeAdotar[indice] = false
-            }
           }
-          // Aqui é bom criar uma function pra não ter que repetir o código e no lugar do for in pode usar indexof, fica melhor
-        }
-      }
-    }
 
-    function AmbasComMesmoAnimal() {
-      for (let i = 0; i < pessoa1PodeAdotar.length; i++) {
-        if (pessoa1PodeAdotar[i] === pessoa2PodeAdotar[i]) {
-          pessoa1PodeAdotar[i] = false
-          pessoa2PodeAdotar[i] = false
-        }
-      }
-    }
-
-    function maisDe3Animais() {
-      let cont = 0
-      for (let i in pessoa1PodeAdotar) {
-        if (pessoa1PodeAdotar[i] === true) {
-          cont += 1
-        }
-        if (cont > 3 && pessoa1PodeAdotar[i] === true) {
-          pessoa1PodeAdotar[i] = false
-        }
+          //Adiciona ao array da pessoa o gato adotado
+          return animaisAdotados.push(animalNome)
       }
 
-      cont = 0
-      for (let i in pessoa2PodeAdotar) {
-        if (pessoa2PodeAdotar[i] === true) {
-          cont += 1
-        }
-        if (cont > 3 && pessoa2PodeAdotar[i] === true) {
-          pessoa2PodeAdotar[i] = false
-        }
-      }
-    }
+      //Função que aplica as regras específicas do jabuti
+      function casoJabuti(brinquedosPessoa, animaisAdotados, animalNome) {     
+          if (casoGeral(brinquedosPessoa, animalNome)) return animaisAdotados.push(animalNome)
 
-    verificarBrinquedosPreferidos();
-    AmbasComMesmoAnimal()
-    loco()
-    maisDe3Animais()
+          //Aplica a lógica de qualquer brinquedo que o jabuti goste, mesmo fora de ordem, ser considerado para adotá-lo
+          if (animaisAdotados.length > 1) {
+              if (listaAnimais[animalNome].brinquedos.every(brinquedo => brinquedosPessoa.includes(brinquedo))) {
+                  return animaisAdotados.push("Loco")  
+              }
+          }
+      }
+
+      //Adiciona ao array da lista de mensagens que serão exibidas
+      function saida(dono, animaisAdotados) {
+          for (let animal of animaisAdotados) {
+              listaMensagens.lista.push(animal + " - " + dono)
+          }
+
+          return listaMensagens.lista.sort()
+      } 
+
+      //Constantes das listas de brinquedos e animais
+      const listaBrinquedos = ["RATO", "BOLA", "LASER", "CAIXA", "NOVELO", "SKATE"]
+      const listaAnimais = {
+        Rex: { animal: "CÃO", brinquedos: ["RATO", "BOLA"] },
+        Mimi: { animal: "GATO", brinquedos: ["BOLA", "LASER"] },
+        Fofo: { animal: "GATO", brinquedos: ["BOLA", "RATO", "LASER"] },
+        Zero: { animal: "GATO", brinquedos: ["RATO", "BOLA"] },
+        Bola: { animal: "CÃO", brinquedos: ["CAIXA", "NOVELO"] },
+        Bebe: { animal: "CÃO", brinquedos: ["LASER", "RATO", "BOLA"] },
+        Loco: { animal: "JABUTI", brinquedos: ["SKATE", "RATO"] }
+      }
+
+      //Mensagens de erros
+      const mensagemErroAnimal = { erro: 'Animal inválido' }
+      const mensagemErroBrinquedo = { erro: 'Brinquedo inválido' }
+
+      //Variáveis que armazenam os animais que foram adotados ou deixaram de ser
+      let adotadosPessoaUm = [] 
+      let adotadosPessoaDois = []
+      let abrigo = []
+
+      //Mensagem final que retorna a lista
+      let listaMensagens = { lista: [] }
+      
+      //Transforma a string dos parâmetros em array
+      brinquedosPessoaUm = transformaEmArray(brinquedosPessoaUm)
+      brinquedosPessoaDois = transformaEmArray(brinquedosPessoaDois)
+      animaisDesejados = transformaEmArray(animaisDesejados)
+
+      //Confere se os animais informados são válidos
+      for (let animal of animaisDesejados) {
+          if (!(animal in listaAnimais)) return mensagemErroAnimal
+      }
     
+      //Chamam as funções de validações
+      if (!confereRepeticao(animaisDesejados)) return mensagemErroAnimal
+      if (!validaBrinquedo(brinquedosPessoaUm) || !confereRepeticao(brinquedosPessoaUm)) return mensagemErroBrinquedo
+      if (!validaBrinquedo(brinquedosPessoaDois) || !confereRepeticao(brinquedosPessoaDois)) return mensagemErroBrinquedo
 
-    // Saídas
-    let saida = []
-    for (let i = 0; i < listaAnimais.length; i++) {
-      if (!pessoa1PodeAdotar[i] && !pessoa2PodeAdotar[i]) {
-        saida.push(`${listaAnimais[i]} - abrigo`)
-      } else if (pessoa1PodeAdotar[i]) {
-        saida.push(`${listaAnimais[i]} - pessoa 1`)
-      } else if (pessoa2PodeAdotar[i]) {
-        saida.push(`${listaAnimais[i]} - pessoa 2`)
+      let pessoaUmPodeAdotar = true
+      let pessoaDoisPodeAdotar = true
+
+      //For que percorre o array dos animais desejados para adoção
+      for (let i = 0; i < animaisDesejados.length; i++) {
+          let animalTipo = listaAnimais[animaisDesejados[i]].animal
+          let animalNome = animaisDesejados[i]
+          let casoGeralPessoaUm = casoGeral(brinquedosPessoaUm, animalNome)
+          let casoGeralPessoaDois = casoGeral(brinquedosPessoaDois, animalNome)
+
+          //Se o número de animais adotados for maior que 3, marca com false
+          if (adotadosPessoaUm.length > 3) pessoaUmPodeAdotar = false
+          if (adotadosPessoaDois.length > 3) pessoaDoisPodeAdotar = false
+
+          //Se ambas as pessoas puderem adotar o mesmo animal, o for passa para o próximo animal e esse animal não é adotado por ninguém
+          //Logo ele ficará no abrigo
+          if (pessoaDoisPodeAdotar && pessoaUmPodeAdotar) {
+              if (casoGeralPessoaUm && casoGeralPessoaDois) continue
+          }
+          
+          //Se o tipo de animal for um cachorro, insere no array se a função geral retornou true
+          if (animalTipo === "CÃO") {
+              if (casoGeralPessoaUm && pessoaUmPodeAdotar) adotadosPessoaUm.push(animalNome)
+              if (casoGeralPessoaDois && pessoaDoisPodeAdotar) adotadosPessoaDois.push(animalNome)
+          } else if (animalTipo === "GATO") {
+              if (pessoaUmPodeAdotar) casoGato(brinquedosPessoaUm, adotadosPessoaUm, animalNome)
+              if (pessoaDoisPodeAdotar) casoGato(brinquedosPessoaDois, adotadosPessoaDois, animalNome)
+          } else {
+              if (pessoaUmPodeAdotar) casoJabuti(brinquedosPessoaUm, adotadosPessoaUm, animalNome)
+              if (pessoaDoisPodeAdotar) casoJabuti(brinquedosPessoaDois, adotadosPessoaDois, animalNome)
+          } 
       }
-    }
-    saida.sort()
+      
+      //Insere no array do abrigo animais que não apareceram em nenhuma dos arrays das pessoas dos animais adotados
+      //Ou se apareceram nos dois arrays
+      for (let animal of animaisDesejados) {
+          if ((!adotadosPessoaUm.includes(animal) && !adotadosPessoaDois.includes(animal)) ||
+              (adotadosPessoaUm.includes(animal) && adotadosPessoaDois.includes(animal))) {
+              abrigo.push(animal)
 
-    let obj = { lista: saida }
-    return obj
+              adotadosPessoaDois = adotadosPessoaDois.filter(pessoaDoisAdotadoFiltrado => pessoaDoisAdotadoFiltrado !== animal)
+              adotadosPessoaUm = adotadosPessoaUm.filter(pessoaUmAdotadoFiltrado => pessoaUmAdotadoFiltrado !== animal)
+          }
+      }
 
+      saida("pessoa 1", adotadosPessoaUm)
+      saida("pessoa 2", adotadosPessoaDois)
+      saida("abrigo", abrigo)
+
+      return listaMensagens
   }
 }
 
 export { AbrigoAnimais as AbrigoAnimais };
-
-// Teste
-const abrigo = new AbrigoAnimais()
-
-console.log(abrigo.encontraPessoas('BOLA,LASER',
-      'BOLA,NOVELO,RATO,LASER', 'Mimi,Fofo,Rex,Bola')) // { lista: [ 'Bola - abrigo', 'Rex - pessoa 1' ] }
-
-// Por que ta retornando undefined
-// Tem que fazer a lógica pra animal duplicado
